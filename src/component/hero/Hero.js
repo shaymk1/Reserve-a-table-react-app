197b31cfebe42cb969f5731f43b7475d3935267e
unknown <mkekae@gmail.com>
2023-02-17 10:40:04 +0200

    hero component finally where i want it to be

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -1,39 +1,44 @@
-import "./hero.css";
+import "./hero.css"
 import heroImg from "../../images/pasta-0.jpg";
 import { Link } from "react-router-dom";
+
+import React from "react";
 
 const Hero = () => {
 	return (
 		<>
 			<section>
-				<div className="container hero__container">
-					<div className="hero-content">
-						<h1>
-							Welcome to Little<span className="lemon"> Lemon</span> , Chicago
-						</h1>
-						<h2>Today 'Special</h2>
-						<p>
-							Pastaranian, our most popular pasta dish with a with a
-							Mediterranean twist. It is made out of the most delicious cheddar
-							cheese flown especially from Greece. Today, you can get it with a{" "}
-							free soft drink of your choice. Perfect for lunch.{" "}
-						</p>
-						<Link to="/reservations" className="btn">
-							Reserve A Table{" "}
-						</Link>
-						
-					</div>
+				<div className="container">
+					<h1>
+						{" "}
+						Welcome to Little<span className="lemon"> Lemon</span> , Chicago
+					</h1>
+					<div className="header__content">
+						<div>
+							<p>
+								Pastaranian, our most popular pasta dish with a with a
+								Mediterranean twist. It is made out of the most delicious
+								cheddar cheese flown especially from Greece. Today, you can get
+								it with a free soft drink of your choice. Perfect for lunch.{" "}
+							</p>
 
-					<div className="hero-image-container">
-						<img
-							src={heroImg}
-							alt="pasta on a plate with sea food and a fork and knife"
-							className="hero-image"
-						/>
+							<p className="price"> $12.99</p>
+							<Link to="/menu">
+								<button className="btn"> Reserve a Table </button>
+							</Link>
+						</div>
+						<div className="hero-img-container">
+							<img
+								className="hero-img"
+								src={heroImg}
+								alt="pasta with seafood on a plate with fork and knive on the side"
+							></img>
+						</div>
 					</div>
 				</div>
 			</section>
 		</>
 	);
 };
+
 export default Hero;
