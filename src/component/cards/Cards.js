173f63c50b573e738d6187808d8eebe8ff9e240e
unknown <mkekae@gmail.com>
2023-02-17 13:07:40 +0200

    modified Cards component media queiries and now fully responsive

--- src/component/cards/Cards.js
+++ src/component/cards/Cards.js
@@ -5,23 +5,23 @@ import { cards } from "../../data";
 const Cards = () => {
 	return (
 		<>
+			<hr></hr>
 			<section>
+				<h2 className="title">Order for delivery</h2>
 				<div className="container card__container">
 					<div className="cards">
 						{cards.map((card) => {
 							const { id, title, img, description, price } = card;
 							return (
 								<div key={id} className="single-card">
-									
 									<img src={img} alt="" className="card-image" />
-									
+
 									<h3>{title}</h3>
 									<p>{description}</p>
 									<p className="price">{price}</p>
 									<Link to="/order" className="btn">
 										Place Order{" "}
 									</Link>
-									
 								</div>
 							);
 						})}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,8 @@
 import "./home.css";
 import { Outlet } from "react-router-dom";
 import Hero from "../../component/hero/Hero";
-import Specials from "../../component/specials/Specials";
+// import Specials from "../../component/specials/Specials";
+import Cards from "../../component/cards/Cards";
 //import Testimonials from "../../component/testimonials/Testimonials";
 //import About from "../../component/about/About"
 
@@ -10,7 +11,8 @@ const Home = () => {
 		<div>
 			<Outlet />
 			<Hero />
-			 <Specials/>  
+			<Cards />
+			
 			{/* <Testimonials/>
             <About/>  */}
 		</div>
