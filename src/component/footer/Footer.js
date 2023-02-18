368907629c12a234eacd8aca41824cb49cc133f8
unknown <mkekae@gmail.com>
2023-02-18 16:04:48 +0200

    started the footer component

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,13 +1,50 @@
-// import "./footer/css"
+import "./footer.css";
+import { links, socials } from "../../data";
+import { Link, NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return (
+	return (
 		<>
 			<hr></hr>
 			<section className="section">
-				<h2>Footer</h2>
+				<div className="footer">
+					<div className="links-container">
+						<ul className="footer-links">
+							{links.map((link) => {
+								const { id, page, path } = link;
+								return (
+									<li key={id}>
+										<NavLink
+											to={path}
+											className={({ isActive }) =>
+												isActive ? "active-nav" : ""
+											}
+											end
+										>
+											{page}
+										</NavLink>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+
+					<div className="icons-container">
+						<ul className="footer-icons">
+							{socials.map((social) => {
+								const { id, text, url, icon } = social;
+								return (
+									<li key={id}>
+										<Link to={url}>{icon}</Link>
+										<Link to={url}>{text}</Link>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
 			</section>
 		</>
 	);
-}
-export default Footer
+};
+export default Footer;
--- src/data.js
+++ src/data.js
@@ -96,7 +96,7 @@ export const navIcons = [
 
 
 
-export const social = [
+export const socials = [
 	{
 		id: 1,
 		url: "https://www.twitter.com",
