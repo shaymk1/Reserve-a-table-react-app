f788fd3f8f83ea76c4480c31b24bd792757b98ad
unknown <mkekae@gmail.com>
2023-02-18 09:27:07 +0200

    finished the responsiveness of about us

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -3,8 +3,8 @@ import React from "react";
 import "./about.css";
 import ownerImgOne from "../../images/woman-1.jpg";
 import ownerImgTwo from "../../images/woman-2-old.jpg";
-//import { FaHandPointer } from "react-icons/fa";
-import { Link } from "react-router-dom";
+
+
 
 const About = () => {
 	return (
@@ -12,19 +12,17 @@ const About = () => {
 			<hr></hr>
 			<section>
 				<h2 className="title">About Us</h2>
-				<h4 className="sub-title">A Little Story..</h4>
+
 				<div className="container about__container">
 					<div className="about-content-container">
+						<h4 className="sub-title">Our Humble Beginnings..</h4>
 						<p>
 							The first Little Lemon restuarant opened it's doors in 2010 in
 							Cape Town, South Africa. It was a brain child of two best friends,
-							who wanted to showcase <span className="ubuntu">Ubuntu</span> to
+							who wanted to showcase <span className="italic">Ubuntu</span> to
 							2010 FIFA Soccer World Cup visitors. Ubuntu is a South African
-							philosophy which means <span className="humanity">Humanity</span>.
+							philosophy which means <span className="italic">Humanity</span>.
 						</p>
-						<Link to="/menu">
-							<button className="btn-primary"> Learn More About Us </button>
-						</Link>
 					</div>
 
 					{/* images */}
@@ -36,25 +34,21 @@ const About = () => {
 								className="about-image-1"
 								src={ownerImgTwo}
 								alt="A lady standing outside  at the balcony with her arms folded at night with her eyes closed smiling or just breathing "
-							></img>
-						
-						
-							{" "}
-							<h4 className="img-title-1">Thandi Kekae </h4>
+							></img>{" "}
+							<h4 className="img-title">Thandi Kekae </h4>
 						</div>
 
 						{/* content-images-2 */}
 
 						{/* images-2 */}
+
 						<div className="image-container">
 							<img
 								className="about-image-2"
 								src={ownerImgOne}
 								alt="A lady standing outside  at the balcony with her arms folded at night with her eyes closed smiling or just breathing "
-							></img>
-					
-							{" "}
-							<h4 className="img-title-1">Shay Brown </h4>
+							></img>{" "}
+							<h4 className="img-title">Shay Brown </h4>
 						</div>
 					</div>
 				</div>
